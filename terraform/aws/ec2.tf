dc3fe3b4fec5531356601796331fa27d9eb0d318
hectorrodriguez-bit <57577440+hectorrodriguez-bit@users.noreply.github.com>
2023-02-01 16:15:55 -0600

    Update ec2.tf

--- terraform/aws/ec2.tf
+++ terraform/aws/ec2.tf
@@ -303,6 +303,6 @@ output "public_subnet" {
 }
 
 output "public_subnet2" {
-  description =    "The ID of the Public subnet"
-  value       = aws_subnet.web_subnet2.id
+  description =    "The ID of the Public subnet"   
+  value       =    aws_subnet.web_subnet2.id
 }
